c9ac6584fcd887bff2d8400c94ef9ae409cde5aa
Naratna <antar.espadas@hotmail.com>
2021-01-30 15:41:43 -0600

    Added IWheel implementation

---
+++ Program/CommandWheelOverlay/Controller/Wheel.cs
@@ -0,0 +1,27 @@
+﻿using CommandWheelOverlay.View;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+
+namespace CommandWheelOverlay.Controller
+{
+    class Wheel : IWheel
+    {
+        public IList<IWheelButton> Buttons { get; set; }
+        public Color AccentColor { get; set; }
+        public Color BgColor { get; set; }
+
+        public SimplifiedWheel Simplify(IList<IWheelButton> buttons)
+        {
+            var buttonsDict = new Dictionary<IWheelButton, int>();
+            for (int i = 0; i < buttons.Count; i++)
+            {
+                buttonsDict.Add(buttons[i], i);
+            }
+            var buttonIndices = Buttons.Select(button => buttonsDict[button]).ToArray();
+
+            return new SimplifiedWheel(buttonIndices, AccentColor, BgColor);
+        }
+    }
+}
