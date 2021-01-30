e5e7ccdeb1af4cd2be75045b85fc494853783d5a
Naratna <antar.espadas@hotmail.com>
2021-01-30 15:56:18 -0600

    Added IWheelAction implementation

---
+++ Program/CommandWheelOverlay/Controller/Actions/OpenProgramAction.cs
@@ -0,0 +1,24 @@
+﻿using CommandWheelOverlay.View;
+using System;
+using System.Diagnostics;
+
+namespace CommandWheelOverlay.Controller.Actions
+{
+    class OpenProgramAction : IWheelAction
+    {
+        public string ProgramPath { get; set; }
+        public string Arguments { get; set; }
+        public void Perform()
+        {
+            Process process = new Process();
+            process.StartInfo.FileName = ProgramPath;
+            process.StartInfo.Arguments = Arguments;
+            process.Start();
+        }
+
+        public SimplifiedWheelAction Simplify()
+        {
+            return new SimplifiedWheelAction(2);
+        }
+    }
+}
