f8a23fa2ca2027814b7d9ccc909d9346d207cb58
Naratna <antar.espadas@hotmail.com>
2021-01-30 15:47:29 -0600

    Added IWheelButton implementation

---
+++ Program/CommandWheelOverlay/Controller/WheelButton.cs
@@ -0,0 +1,20 @@
+﻿using CommandWheelOverlay.View;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommandWheelOverlay.Controller
+{
+    class WheelButton : IWheelButton
+    {
+        public string Label { get; set; } = "Button";
+        public string ImgPath { get; set; } = null;
+        public IWheel SubWheel { get; set; } = null;
+        public IWheelAction Action { get; set; } = null;
+
+        public SimplifiedWheelButton Simplify(IList<IWheel> wheels)
+        {
+            return new SimplifiedWheelButton(Label, ImgPath, wheels.IndexOf(SubWheel), Action.Simplify());
+        }
+    }
+}
