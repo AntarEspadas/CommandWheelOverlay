8adc54e13eecfb5d3ef2dbf4afaecef3584d056a
Naratna <antar.espadas@hotmail.com>
2021-01-30 08:21:00 -0600

    Updated cursor script to use new api

--- Overlay/Assets/Scripts/CursorMovement.cs
+++ Overlay/Assets/Scripts/CursorMovement.cs
@@ -4,56 +4,40 @@ using System.Net.Sockets;
 using System.Runtime.Serialization.Formatters.Binary;
 using Linearstar.Windows.RawInput;
 using UnityEngine;
+using CommandWheelOverlay.Connection;
+using CommandWheelOverlay.View;
+using CommandWheelOverlay.Input;
+using CommandWheelOverlay.Settings;
 
 public class CursorMovement : MonoBehaviour
 {
     public RectTransform constraint;
-    public int sensitivity;
-    private Vector2 movement = new Vector2(0, 0);
-    private TcpListener listener = new TcpListener(IPAddress.Any, 25565);
-    private Socket client = null;
-    private NetworkStream stream;
-    private BinaryFormatter formatter;
+    public float sensitivity;
+    private static Vector2 movement = new Vector2(0, 0);
+    private TcpOverlayController controller;
     // Start is called before the first frame update
 
     void Start()
     {
         Cursor.visible = false;
-        listener.Start();
+        OverlayView view = new OverlayView(sensitivity);
+        controller = new TcpOverlayController(view, 7777);
+#if !UNITY_EDITOR
+        controller.Connect();
+#endif
     }
-    
+
     // Update is called once per frame
     void Update()
     {
-        if (client is null && listener.Pending())
-        {
-            client = listener.AcceptSocket();
-            stream = new NetworkStream(client);
-            formatter = new BinaryFormatter();
-        }
-        if (client != null)
-        {
-            while (stream.DataAvailable)
-            {
-                object line = formatter.Deserialize(stream);
-                if (line is long[] data)
-                {
-                    movement.x = data[0] * sensitivity;
-                    movement.y = data[1] * -sensitivity;
-                    //movement = new Vector2(data[0], data[1] * -1) * (speed / 10);
-                    Debug.Log($"{data[0]}, {data[1]}");
-                }
-            }
-        }
-        else
-        {
-            movement.x += Input.GetAxis("Mouse X") * sensitivity * 5;
-            movement.y += Input.GetAxis("Mouse Y") * sensitivity * 5;
-        }
-
         var rect = constraint.rect;
 
         Vector3 pos = transform.position;
+
+#if UNITY_EDITOR
+        movement.x += Input.GetAxis("Mouse X") * sensitivity * 10;
+        movement.y += Input.GetAxis("Mouse Y") * sensitivity * 10;
+#endif
 
         float xMin = rect.x + rect.width / 2;
         float xMax = rect.x - rect.width / 2;
@@ -70,5 +54,34 @@ public class CursorMovement : MonoBehaviour
         movement.x = 0;
         movement.y = 0;
 
+    }
+
+    class OverlayView : IOverlayView
+    {
+        private float sensitivity;
+        public OverlayView(float sensitivity)
+        {
+            this.sensitivity = sensitivity;
+        }
+        public void SendInput(KeyboardInput input)
+        {
+            return;
+        }
+
+        public void SendInput(MouseInput input)
+        {
+            movement.x += input.LastX * sensitivity;
+            movement.y -= input.LastY * sensitivity;
+        }
+
+        public void UpdateElements(ISimplifiedWheelElements elements)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void UpdateSettings(IUserSettings settings)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
