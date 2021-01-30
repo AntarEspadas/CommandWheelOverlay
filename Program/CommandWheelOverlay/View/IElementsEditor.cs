397226a82eeea406b990cace6b3c198d367f0644
Naratna <antar.espadas@hotmail.com>
2021-01-30 08:59:31 -0600

    Added editor interfaces

---
+++ Program/CommandWheelOverlay/View/IElementsEditor.cs
@@ -0,0 +1,12 @@
+﻿using CommandWheelOverlay.Controller;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommandWheelOverlay.View
+{
+    public interface IElementsEditor
+    {
+        IWheelElements Edit(IWheelElements wheelElementsCopy);
+    }
+}
---
+++ Program/CommandWheelOverlay/View/ISettingsEditor.cs
@@ -0,0 +1,12 @@
+﻿using CommandWheelOverlay.Settings;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommandWheelOverlay.View
+{
+    public interface ISettingsEditor
+    {
+        IUserSettings EditSettings(IUserSettings userSettingsCopy);
+    }
+}
