836feb5e48ac2d6afab567b5ff9c368e7a1a1d89
Naratna <antar.espadas@hotmail.com>
2021-01-31 00:46:01 -0600

    Added implementation of IWheelElements

---
+++ Program/CommandWheelOverlay/Controller/WheelElements.cs
@@ -0,0 +1,57 @@
+﻿using CommandWheelOverlay.View;
+using CommandWheelOverlay.View.Editors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CommandWheelOverlay.Controller
+{
+    class WheelElements : IWheelElements
+    {
+        public IElementsEditor Editor { get; set; }
+        public IList<IWheel> Wheels { get; set; }
+        public IList<IWheelButton> Buttons { get; set; }
+
+        public object Clone()
+        {
+            WheelElements clone = new WheelElements
+            {
+                Wheels = new List<IWheel>(Wheels.Count),
+                Buttons = new List<IWheelButton>(Buttons.Count)
+            };
+            var wheelsDict = new Dictionary<IWheel, int>();
+            var buttonsDict = new Dictionary<IWheelButton, int>();
+            for (int i = 0; i < Wheels.Count; i++)
+            {
+                wheelsDict[Wheels[i]] = i;
+                clone.Wheels.Add((IWheel)Wheels[i].Clone());
+            }
+            for (int i = 0; i < Buttons.Count; i++)
+            {
+                buttonsDict[Buttons[i]] = i;
+                clone.Buttons.Add((IWheelButton)Buttons[i].Clone());
+            }
+            foreach (IWheel wheel in clone.Wheels)
+                for (int i = 0; i < wheel.Buttons.Count; i++)
+                {
+                    IWheelButton button = wheel.Buttons[i];
+                    int buttonIndex = buttonsDict[button];
+                    wheel.Buttons[i] = clone.Buttons[buttonIndex];
+                }
+            foreach (IWheelButton button in clone.Buttons)
+            {
+                int wheelIndex = wheelsDict[button.SubWheel];
+                button.SubWheel = clone.Wheels[wheelIndex];
+            }
+            return clone;
+        }
+
+        public SimplifiedWheelElements Simplify()
+        {
+            var simplifiedWheels = Wheels.Select(element => element.Simplify(Buttons)).ToArray();
+            var simplifiedButtons = Buttons.Select(element => element.Simplify(Wheels)).ToArray();
+            return new SimplifiedWheelElements(simplifiedWheels, simplifiedButtons);
+        }
+    }
+}
