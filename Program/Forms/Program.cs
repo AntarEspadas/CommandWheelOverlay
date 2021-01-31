006f3de47033fb9d94001a94d72aa50cfa78f5b3
Naratna <antar.espadas@hotmail.com>
2021-01-31 00:50:46 -0600

    Added missing application function calls

--- Program/Forms/Program.cs
+++ Program/Forms/Program.cs
@@ -14,6 +14,8 @@ namespace CommandWheelForms
     {
         public static void Main(string[] args)
         {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainWindow());
         }
     }
