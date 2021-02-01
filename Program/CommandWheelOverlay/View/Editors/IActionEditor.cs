64abc3e3899dc5c8800f05ae1270aafe2d12f627
Naratna <antar.espadas@hotmail.com>
2021-02-01 03:57:19 -0600

    Updated IActionEditor

--- Program/CommandWheelOverlay/View/Editors/IActionEditor.cs
+++ Program/CommandWheelOverlay/View/Editors/IActionEditor.cs
@@ -7,23 +7,24 @@ namespace CommandWheelOverlay.View.Editors
 {
     public interface IActionEditor
     {
+        Type Type { get; }
         string DisplayName { get; }
 
         IWheelAction CreateAction();
-        IWheelAction EditAction(IWheelAction action);
+        bool EditAction(IWheelAction action);
     }
 
-    public abstract class IActionEditor<T> : IActionEditor where T : IWheelAction
+    public abstract class ActionEditor<T> : IActionEditor where T : IWheelAction
     {
         public Type Type { get => typeof(T); }
         public abstract string DisplayName { get; }
 
         public abstract T CreateAction();
 
-        public abstract T EditAction(IWheelAction action);
+        public abstract bool EditAction(T action);
 
         IWheelAction IActionEditor.CreateAction() => CreateAction();
 
-        IWheelAction IActionEditor.EditAction(IWheelAction action) => EditAction(action);
+        bool IActionEditor.EditAction(IWheelAction action) => EditAction((T)action);
     }
 }
