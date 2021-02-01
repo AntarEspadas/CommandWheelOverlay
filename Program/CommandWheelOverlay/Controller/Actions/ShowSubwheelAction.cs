ba4573cb6c730f243e10005803c908e5fd8397b5
Naratna <antar.espadas@hotmail.com>
2021-02-01 03:05:33 -0600

    Added ShowSubwheelAction

---
+++ Program/CommandWheelOverlay/Controller/Actions/ShowSubwheelAction.cs
@@ -0,0 +1,25 @@
+﻿using CommandWheelOverlay.View;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommandWheelOverlay.Controller.Actions
+{
+    public class ShowSubwheelAction : IWheelAction
+    {
+        public object Clone()
+        {
+            return MemberwiseClone();
+        }
+
+        public void Perform()
+        {
+            throw new NotSupportedException();
+        }
+
+        public SimplifiedWheelAction Simplify()
+        {
+            return new SimplifiedWheelAction(WheelActionType.DisplaySubwheel);
+        }
+    }
+}
