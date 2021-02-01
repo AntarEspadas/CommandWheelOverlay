7cace2b2a5e105c88cb3f07c8e7137a261b07e85
Naratna <antar.espadas@hotmail.com>
2021-02-01 05:09:00 -0600

    Added control for basic form layout

---
+++ Program/Forms/Controls/BasicLayout.Designer.cs
@@ -0,0 +1,128 @@
+﻿
+namespace CommandWheelForms.Controls
+{
+    partial class BasicLayout
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainTable = new System.Windows.Forms.TableLayoutPanel();
+            this.buttonTable = new System.Windows.Forms.TableLayoutPanel();
+            this.okButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.mainPanel = new System.Windows.Forms.Panel();
+            this.mainTable.SuspendLayout();
+            this.buttonTable.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mainTable
+            // 
+            this.mainTable.ColumnCount = 1;
+            this.mainTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.mainTable.Controls.Add(this.buttonTable, 0, 1);
+            this.mainTable.Controls.Add(this.mainPanel, 0, 0);
+            this.mainTable.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainTable.Location = new System.Drawing.Point(0, 0);
+            this.mainTable.Name = "mainTable";
+            this.mainTable.RowCount = 2;
+            this.mainTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.mainTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.mainTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.mainTable.Size = new System.Drawing.Size(335, 243);
+            this.mainTable.TabIndex = 0;
+            // 
+            // buttonTable
+            // 
+            this.buttonTable.ColumnCount = 5;
+            this.buttonTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.buttonTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 80F));
+            this.buttonTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.buttonTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 80F));
+            this.buttonTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.buttonTable.Controls.Add(this.okButton, 1, 0);
+            this.buttonTable.Controls.Add(this.cancelButton, 3, 0);
+            this.buttonTable.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonTable.Location = new System.Drawing.Point(3, 206);
+            this.buttonTable.Name = "buttonTable";
+            this.buttonTable.RowCount = 1;
+            this.buttonTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.buttonTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.buttonTable.Size = new System.Drawing.Size(329, 34);
+            this.buttonTable.TabIndex = 0;
+            // 
+            // okButton
+            // 
+            this.okButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.okButton.Location = new System.Drawing.Point(45, 3);
+            this.okButton.Name = "okButton";
+            this.okButton.Size = new System.Drawing.Size(74, 28);
+            this.okButton.TabIndex = 0;
+            this.okButton.Text = "Ok";
+            this.okButton.UseVisualStyleBackColor = true;
+            this.okButton.Click += new System.EventHandler(this.OkButton_Click);
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.cancelButton.Location = new System.Drawing.Point(209, 3);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(74, 28);
+            this.cancelButton.TabIndex = 1;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.Click += new System.EventHandler(this.CancelButton_Click);
+            // 
+            // mainPanel
+            // 
+            this.mainPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainPanel.Location = new System.Drawing.Point(3, 3);
+            this.mainPanel.Name = "mainPanel";
+            this.mainPanel.Size = new System.Drawing.Size(329, 197);
+            this.mainPanel.TabIndex = 1;
+            // 
+            // BasicLayout
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.mainTable);
+            this.Margin = new System.Windows.Forms.Padding(0);
+            this.Name = "BasicLayout";
+            this.Size = new System.Drawing.Size(335, 243);
+            this.mainTable.ResumeLayout(false);
+            this.buttonTable.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel mainTable;
+        private System.Windows.Forms.TableLayoutPanel buttonTable;
+        private System.Windows.Forms.Button okButton;
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.Panel mainPanel;
+    }
+}
---
+++ Program/Forms/Controls/BasicLayout.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CommandWheelForms.Controls
+{
+    public partial class BasicLayout : UserControl
+    {
+        public event EventHandler ClickOkButton;
+        public event EventHandler ClickCancelButton;
+
+        public BasicLayout()
+        {
+            InitializeComponent();
+        }
+
+        private void OkButton_Click(object sender, EventArgs e)
+        {
+            ClickOkButton?.Invoke(sender, e);
+            ParentForm.DialogResult = DialogResult.OK;
+            ParentForm.Close();
+        }
+
+        private void CancelButton_Click(object sender, EventArgs e)
+        {
+            ClickCancelButton?.Invoke(sender, e);
+            ParentForm.DialogResult = DialogResult.Cancel;
+            ParentForm.Close();
+        }
+    }
+}
