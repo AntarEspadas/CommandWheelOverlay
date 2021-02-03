ab6b5899dd28f367e276bb5a1b7f00dc172e5bc9
Naratna <antar.espadas@hotmail.com>
2021-02-03 14:57:11 -0600

    Added highlight on hover

---
+++ Overlay/Assets/Scripts/CursorHighlight.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CursorHighlight : MonoBehaviour
+{
+    public Wheel wheel;
+    public RectTransform test;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var cursorPosition = transform.localPosition - wheel.transform.localPosition;
+        var wheelTransform = wheel.transform as RectTransform;
+        var a = wheel.transform.TransformVector(wheelTransform.sizeDelta);
+        var b = transform.parent.InverseTransformVector(a);
+        Debug.Log(transform.localPosition.magnitude);
+        if (wheel.ActualRadious >= cursorPosition.magnitude)
+        {
+            float buttonAngle = 360f / wheel.buttons;
+            float cursorAngle = Angle360(wheel.StartVector, cursorPosition);
+            int index = (int)(cursorAngle / buttonAngle);
+            wheel.Highlighted = index;
+        }
+        else
+        {
+            wheel.Highlighted = -1;
+        }
+    }
+    private static float Angle360(Vector2 start, Vector2 point)
+    {
+        float angle = Vector2.SignedAngle(start, point);
+        return angle + (angle < 0 ? 360 : 0);
+    }
+}
--- Overlay/Assets/Scripts/Wheel.cs
+++ Overlay/Assets/Scripts/Wheel.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 //[ExecuteInEditMode()]
 public class Wheel : MonoBehaviour
@@ -10,6 +11,14 @@ public class Wheel : MonoBehaviour
     public float innerRadious;
     public RectTransform separator;
     public WheelSegment segment;
+    private WheelSegment[] childSegments;
+
+    public int Highlighted { get => highlighted; set => SetHighlighted(value); }
+    public Vector2 StartVector { get; private set; }
+    public float InnerRadiousDelta { get; private set; }
+    public float ActualRadious { get; private set; }
+    private int highlighted = -1;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,10 +26,12 @@ public class Wheel : MonoBehaviour
         transform.localScale = new Vector2(innerRadious * 2, innerRadious * 2);
         float angleDiff = 360f / buttons;
         float offset = angleDiff / 2;
+        childSegments = new WheelSegment[buttons];
         for (int i = 0; i < buttons; i++)
         {
-            float angle = angleDiff * i - offset;
+            float angle = angleDiff * i + offset;
             var seg = Instantiate(segment, transform.position, Quaternion.Euler(0, 0, angle), mask);
+            childSegments[i] = seg;
             seg.degrees = angleDiff;
             seg.index = i;
             seg.radious = radious;
@@ -36,11 +47,31 @@ public class Wheel : MonoBehaviour
             sep.sizeDelta = separatorSize;
             sep.transform.localPosition += sep.up * (sep.rect.height / 2);
         }
+        StartVector = childSegments[buttons - 1].transform.up;
+        InnerRadiousDelta = ((RectTransform)mask).sizeDelta.x;
+        ActualRadious =  ((RectTransform)transform).sizeDelta.y * radious;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void SetHighlighted(int value)
+    {
+        if (value == highlighted) return;
+        Transform highlight;
+        if (highlighted >= 0)
+        {
+            highlight = childSegments[highlighted].transform.Find("Highlight");
+            highlight.GetComponent<Image>().enabled = false;
+        }
+        if (value >= 0)
+        {
+            highlight = childSegments[value].transform.Find("Highlight");
+            highlight.GetComponent<Image>().enabled = true;
+        }
+        highlighted = value;
     }
 }
