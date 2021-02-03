31fb6b1b19057f01c6f233e862cc3855afe3d0a0
Naratna <antar.espadas@hotmail.com>
2021-02-03 05:51:45 -0600

    Added center mask

--- Overlay/Assets/Scripts/Wheel.cs
+++ Overlay/Assets/Scripts/Wheel.cs
@@ -12,13 +12,14 @@ public class Wheel : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        var mask = transform.Find("Mask");
         transform.localScale = new Vector2(innerRadious * 2, innerRadious * 2);
         float angleDiff = 360f / buttons;
         float offset = angleDiff / 2;
         for (int i = 0; i < buttons; i++)
         {
             float angle = angleDiff * i + offset;
-            var t = Instantiate(obj, transform.position, Quaternion.Euler(0, 0, angle), transform);
+            var t = Instantiate(obj, transform.position, Quaternion.Euler(0, 0, angle), mask);
             var separatorSize = t.sizeDelta;
             separatorSize.x /= transform.localScale.x;
             separatorSize.y /= transform.localScale.y * radious;
