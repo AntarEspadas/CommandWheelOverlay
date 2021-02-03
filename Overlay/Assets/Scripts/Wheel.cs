ac1436f080f0165e0a2710472625b2a4950e37c0
Naratna <antar.espadas@hotmail.com>
2021-02-03 05:34:16 -0600

    Improved wheel math

--- Overlay/Assets/Scripts/Wheel.cs
+++ Overlay/Assets/Scripts/Wheel.cs
@@ -6,18 +6,24 @@ using UnityEngine;
 public class Wheel : MonoBehaviour
 {
     public int buttons;
+    public float radious;
+    public float innerRadious;
     public RectTransform obj;
     // Start is called before the first frame update
     void Start()
     {
+        transform.localScale = new Vector2(innerRadious * 2, innerRadious * 2);
         float angleDiff = 360f / buttons;
         float offset = angleDiff / 2;
         for (int i = 0; i < buttons; i++)
         {
             float angle = angleDiff * i + offset;
-            var t = Instantiate(obj, transform, true);
-            t.position = transform.position + (Vector3)VectorFromPolar(angle + 90, 50);
-            t.rotation = Quaternion.Euler(0, 0, angle);
+            var t = Instantiate(obj, transform.position, Quaternion.Euler(0, 0, angle), transform);
+            var separatorSize = t.sizeDelta;
+            separatorSize.x /= transform.localScale.x;
+            separatorSize.y /= transform.localScale.y * radious;
+            t.sizeDelta = separatorSize;
+            t.transform.localPosition += t.up * (t.rect.height / 2);
         }
     }
 
@@ -25,14 +31,5 @@ public class Wheel : MonoBehaviour
     void Update()
     {
         
-    }
-
-    private Vector2 VectorFromPolar(float theta, float r)
-    {
-        float radians = 2 * Mathf.PI * theta;
-        radians /= 360;
-        float x = Mathf.Cos(radians) * r;
-        float y = Mathf.Sin(radians) * r;
-        return new Vector2(x, y);
     }
 }
