1d6751ef0d8dff7a4473c61dcea63f3a83c1eb46
Naratna <antar.espadas@hotmail.com>
2021-02-04 03:16:16 -0600

    Added IInputHandler

---
+++ Program/CommandWheelOverlay/Input/IInputHandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommandWheelOverlay.Input
+{
+    interface IInputHandler
+    {
+        void Handle(MouseInput input);
+        void Handle(KeyboardInput input);
+    }
+}
