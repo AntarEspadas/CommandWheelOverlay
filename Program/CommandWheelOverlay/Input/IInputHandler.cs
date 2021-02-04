8eeea00e7a5d03b690092672bbb36de223560e9a
Naratna <antar.espadas@hotmail.com>
2021-02-04 03:49:20 -0600

    Updated interface

--- Program/CommandWheelOverlay/Input/IInputHandler.cs
+++ Program/CommandWheelOverlay/Input/IInputHandler.cs
@@ -1,11 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
 
 namespace CommandWheelOverlay.Input
 {
     interface IInputHandler
     {
+        IList<int> ShowHotkey { get; set; }
+        IList<int> MoveLeftHotkey { get; set; }
+        IList<int> MoveRightHotkey { get; set; }
+        void RecordHotkey(Action<int> keyRecordedCallback, Action<IList<int>> finishedRecordingCallback, CancellationToken cancellationToken);
         void Handle(MouseInput input);
         void Handle(KeyboardInput input);
     }
