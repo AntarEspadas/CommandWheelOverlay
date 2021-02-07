a0551e32a0dccc5ef492e998bbb189d46b43dc26
Naratna <antar.espadas@hotmail.com>
2021-02-07 12:04:32 -0600

    Added model

---
+++ Program/CommandWheelOverlay/Model/IOverlayModel.cs
@@ -0,0 +1,17 @@
+﻿using CommandWheelOverlay.Controller;
+using CommandWheelOverlay.Settings;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommandWheelOverlay.Model
+{
+    interface IOverlayModel
+    {
+        IWheelElements GetElements();
+        bool SaveElements(IWheelElements elements);
+
+        IUserSettings GetSettings();
+        bool SaveSettings(IUserSettings settings);
+    }
+}
