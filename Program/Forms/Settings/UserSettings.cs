fefc87e4371760e4e8ed2c15bd8695c3702bf7d6
Naratna <antar.espadas@hotmail.com>
2021-02-08 16:11:29 -0600

    Added save method

--- Program/Forms/Settings/UserSettings.cs
+++ Program/Forms/Settings/UserSettings.cs
@@ -10,12 +10,20 @@ namespace CommandWheelForms.Settings
 {
     class UserSettings : IUserSettings
     {
+        public static UserSettings Instance { get => instance; }
+        private static UserSettings instance = new UserSettings();
+
         private readonly Properties.Settings settings = Properties.Settings.Default;
 
         public ISettingsEditor Editor { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
         public IList<int> ShowHotkey { get => settings.ShowHotkey.ToList(); set => settings.ShowHotkey = value.ToArray(); }
         public IList<int> MoveLeftHotkey { get => settings.MoveLeftHotkey.ToList(); set => settings.MoveLeftHotkey = value.ToArray(); }
         public IList<int> MoveRightHotkey { get => settings.MoveRightHotkey.ToList(); set => settings.MoveRightHotkey = value.ToArray(); }
+
+        private UserSettings()
+        {
+
+        }
 
         public void Save()
             => settings.Save();
