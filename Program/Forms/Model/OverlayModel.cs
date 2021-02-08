ef3106a58d15473c0ecf62277983f2019d1bc331
Naratna <antar.espadas@hotmail.com>
2021-02-08 16:14:55 -0600

    Added Model

---
+++ Program/Forms/Model/OverlayModel.cs
@@ -0,0 +1,32 @@
+﻿using CommandWheelForms.Settings;
+using CommandWheelOverlay.Model;
+using CommandWheelOverlay.Settings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandWheelForms.Model
+{
+    class OverlayModel : JsonElementsModel
+    {
+        public override IUserSettings GetSettings()
+        {
+            return UserSettings.Instance;
+        }
+
+        public override bool SaveSettings(IUserSettings settings)
+        {
+            try
+            {
+                ((UserSettings)settings).Save();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
