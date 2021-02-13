2e8d1b943c6913484b8adf529a117ccd2bdbc704
Naratna <antar.espadas@hotmail.com>
2021-02-13 12:13:56 -0600

    Updated assembly information

--- Program/Forms/Properties/AssemblyInfo.cs
+++ Program/Forms/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MouseInput")]
+[assembly: AssemblyTitle("CommandWheel")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MouseInput")]
+[assembly: AssemblyProduct("CommandWheel")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
