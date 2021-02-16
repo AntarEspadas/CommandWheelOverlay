3631780554801d849afbb2d12b37e8a575222439
Naratna <antar.espadas@hotmail.com>
2021-02-15 19:57:51 -0600

    Removed subwheel editor from editors list

--- Program/Forms/Forms/MainWindow.cs
+++ Program/Forms/Forms/MainWindow.cs
@@ -49,7 +49,7 @@ namespace CommandWheelForms.Forms
             {
                 WheelEditor = new WheelEditor<Wheel>(),
                 ButtonEditor = new ButtonEditor<WheelButton>(),
-                ActionEditors = new List<IActionEditor>() { new ShowSubwheelActionEditor(), new OpenProgramEditor() },
+                ActionEditors = new List<IActionEditor>() { new OpenProgramEditor() },
             };
 
 
