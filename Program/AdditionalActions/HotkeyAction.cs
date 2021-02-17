6ddb19e670891b4faabeab92eeb760b9d6288473
Naratna <antar.espadas@hotmail.com>
2021-02-17 13:12:21 -0600

    Added HotkeyAction

---
+++ Program/AdditionalActions/HotkeyAction.cs
@@ -0,0 +1,38 @@
+﻿using CommandWheelOverlay.Controller;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using sharpAHK;
+using System.Collections.ObjectModel;
+
+namespace CommandWheelOverlay.AdditionalActions
+{
+    class HotkeyAction : IWheelAction
+    {
+        private static readonly _AHK _ahk = new _AHK();
+
+        private string _hotkey;
+
+        public void SetHotkey(params string[] keys)
+        {
+            string downs = "";
+            string ups = "";
+            foreach (string input in keys)
+            {
+                downs += "{" + input + " Down}";
+                ups += "{" + input + " Up}";
+            }
+            _hotkey = downs + ups;
+        }
+
+        public IWheelAction Clone(IWheelElements elements)
+        {
+            return (IWheelAction)MemberwiseClone();
+        }
+
+        public void Perform()
+        {
+            _ahk.SendInput(_hotkey);
+        }
+    }
+}
